21967c037958ca8e8b39b69724e6655d749fbb7d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-03-30 20:35:32 -0700

    Start

---
+++ Demo Swift CoreML Vision API/Demo Swift CoreML Vision API/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  Demo Swift CoreML Vision API
+//
+//  Created by Joel on 3/30/18.
+//  Copyright © 2018 JoelParkerHenderson.com. All rights reserved.
+//
+
+import UIKit
+import AVCaptureSession
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+        view.addSubview(modelPredictionLabel)
+        setupModelPredictionLabel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    let modelPredictionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Label"
+        label.font = label.font.withSize(30)
+        return label
+    }()
+    
+    func setupModelPredictionLabel() {
+        modelPredictionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        modelPredictionLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+    
+    func setupCaptureSession() {
+
+        // Get a reference to the rear view camera.
+        // Use a DiscoverySession to query available
+        // capture devices based on search criteria.
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // The AVCaptureSession object handles capture activity and manages
+        // the flow of data between input devices (such as the camera) and outputs.
+        let captureSession = AVCaptureSession()
+    
+        // get capture device, add device input to capture session
+        do {
+            // The first available device will be the rear facing camera.
+            if let captureDevice = availableDevices.first {
+                // Ceate a new AVCaptureDeviceInput using our capture device,
+                // and add it to the capture session.
+                captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to our capture session
+        
+        // AVCaptureVideoDataOutput is an output that captures video.
+        // It also provides access to the frames being captured for
+        // processing with a delegate method we will see later.
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(captureOutput)
+
+        // Add capture session output as a sublayer to the view controllers’ view.
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+
+        // Starts the flow from inputs to outputs.
+        captureSession.startRunning()
+        
+        // To analyze what the camera is seeing, we need access
+        // to the frames being captured by the camera.
+        //
+        // Conforming to the AVCaptureVideoDataOutputSampleBufferDelegate
+        // gives an interface for interation, and gives a notification
+        // every time a frame is captured by the camera.
+        //
+        // Each time a frame is captured, the delegate is notified by
+        // calling captureOutput() which does image analysis with CoreML.
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+ 
+        // Create a VNCoreMLModel which is essentially a
+        // CoreML model used with the vision framework.
+        // We create it with a Resnet50 Model.
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+
+        // Create our vision request. In the completion handler,
+        // we update the onscreen UILabel with the identifier
+        // returned by the model.
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            DispatchQueue.main.async(execute: {
+                self.label.text = "\(Observation.identifier)"
+            })
+        }
+        
+        // Convert the frame from a CMSampleBuffer to a CVPixelBuffer,
+        // which is the format that our model needs for analysis.
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // Executes the Vision request with a VNImageRequestHandler
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,125 @@
+# Demo Swift Hello World
+
+<img src="README.png" alt="Hello World" style="width: 100%;"/>
+
+This demonstration shows:
+
+  * [Swift](http://swift.org) programming language with
+    [Apple](http://apple.com)
+    [Xcode](https://developer.apple.com/xcode/)
+    [iOS](http://www.apple.com/ios/)
+
+  * How to create a view that shows "Hello World" on the screen.
+
+To use this project:
+
+  * You can clone this repo.
+
+  * Or you can create the project yourself by using this README that explains how to do it.
+
+
+## Create the project
+
+Launch Xcode. 
+
+Choose "File" → "New" → "Project". 
+
+You see the dialog "Choose a template for your new project".
+
+  * Choose the "iOS" radio button.
+
+  * Choose the "Single View Application" icon.
+
+  * Tap the button "Next".
+
+You see the dialog "Choose options for your new project".
+
+  * For "Product Name", type "Demo Swift CoreML Vision API".
+
+  * For "Organization Name", type anything you want. Typically this is your company's name, or team's name, or similiar. For example, "Example Company".
+
+  * For "Organization Identifier", type anything you want. Typically this is your company's reverse domain name. For example, "com.example".
+
+  * Tap the button "Next".
+
+You see the file chooser.
+
+  * Choose where to save your project.
+
+  * Tap the button "Create".
+
+  * Xcode creates the project.
+
+
+## Sign the project
+
+If Xcode shows a "Signing" area with a "Status" alert icon, here's how to solve it.
+
+  * Xcode may show a message such as "Signing for "Demo Swift Hello World" requires a development team. Select a development team in the project editor."
+
+  * In the same "Signing" section, choose the "Team" dropdown, and choose your Apple ID.
+
+  * If there's no Apple ID, then you need to add yours: Xcode → Preferences → Accounts → the "+" button → Add Apple ID
+
+
+## Run the project
+
+Run the project for the first time.
+
+  * Xcode → Product → Run
+
+  * This is simply to verify that the project runs so far.
+
+The Simulator launches.
+
+  * The Simulator shows a blank screen.
+
+  * Quit the Simulator and go back to using Xcode.
+
+
+## Create the project
+
+Use this: https://medium.freecodecamp.org/ios-coreml-vision-image-recognition-3619cf319d0b
+
+Delete the storyboard:
+
+  * Delete the file "Main.storyboard".
+
+  * Select the file "Info.plist".
+
+  * The main area shows "Information Property List".
+
+  * One entry is "Main storyboard file base name" and set to "Main".
+
+  * Delete the entry.
+
+Add the machine learning model:
+
+  * Download 
+
+  * Add the model to your Xcode project by dragging the model into the project navigator.
+
+  * You can see information about the model—including the model type and its expected inputs and outputs—by opening the model in Xcode. The inputs to the model are the number of solar panels and greenhouses, as well as the lot size of the habitat (in acres). The output of the model is the predicted price of the habitat.
+
+  * Xcode also uses information about the model’s inputs and outputs to automatically generate a custom programmatic interface to the model, which you use to interact with the model in your code. For a model named "Foo" with a file name "Foo.mlmodel", Xcode generates interfaces to represent the model ("Foo"), the model’s inputs ("FooInput"), and the model’s output ("FooOutput").
+
+
+## Run
+
+Run the project.
+
+  * Xcode → Product → Run
+
+The Simulator launches.
+
+  * Congratulations! You're successful!
+  
+
+## Tracking
+
+* Package: demo_swift_coreml_vision_api
+* Version: 1.0.0
+* Created: 2018-03-29
+* Updated: 2018-03-30
+* License: BSD, GPL, MIT
+* Contact: Joel Parker Henderson (http://joelparkerhenderson.com)
